c75884292e3ce1522cec22081f62eb5bf6734d20
maxbondarenko <bondarenkomm@bk.ru>
2022-11-27 00:46:27 +0500

    zadacha29

---
+++ zadacha29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов 
+// и выводит их на экран. Вывод сделать отдельным методом.
+// 5 -> [1, 2, 5, 7, 19]
+// 3 -> [6, 1, 33]
+
+int[] array = new int[5];
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i<array.Length; i++)
+    {
+    array[i] = new Random().Next(1,5);
+    Console.Write(array[i] + " ");
+    }
+}
+PrintArray(array);
+
+
+
